68389dff1caba3208ac50e88574bd12132f0984a
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2022-01-03 21:31:20 -0500

    Add utility files

---
+++ battlecode22-scaffold/src/FirstBot/utility/Globals.java
@@ -0,0 +1,39 @@
+package utility;
+
+public class Globals {
+
+    // public static RobotController rc;
+	// public static MapLocation here;
+	// public static Team us;
+	// public static Team them;
+	// public static int myID;
+	// public static RobotType myType;
+    public static int roundNum;
+
+    // public static RobotInfo[] visibleHostiles = null;
+	// public static RobotInfo[] visibleEnemies = null;
+	// public static RobotInfo[] visibleZombies = null;
+	// public static RobotInfo[] visibleAllies = null;
+	// public static RobotInfo[] attackableHostiles = null;
+
+    // public static void init(RobotController theRC) {
+	// 	rc = theRC;
+	// 	us = rc.getTeam();
+	// 	them = us.opponent();
+	// 	myID = rc.getID();
+	// 	myType = rc.getType();
+	// 	here = rc.getLocation();
+	// }
+
+    // public static void update() {
+	// 	here = rc.getLocation();
+	// 	roundNum = rc.getRoundNum();
+	// }
+
+    // public static void updateRobotInfos() {
+		// visibleAllies = rc.senseNearbyRobots(mySensorRadiusSquared, us);
+		// visibleEnemies =  rc.senseNearbyRobots(mySensorRadiusSquared, them);
+		// visibleHostiles = rc.senseHostileRobots(here, mySensorRadiusSquared);
+		// attackableHostiles = rc.senseHostileRobots(here, myAttackRadiusSquared);
+	// }
+}
