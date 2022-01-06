5684302ee7f2801320936bbead622c0a41fa1b65
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2022-01-06 16:47:02 -0500

    Introducing CommBot

---
+++ battlecode22-scaffold/src/CommBot/PathFinder.java
@@ -0,0 +1,18 @@
+package CommBot;
+
+import battlecode.common.*;
+
+public class PathFinder extends Util {
+
+    private static Heap minHeap;
+
+    public static void initPathFinder(){
+        int doNothing;
+    }
+
+    public static Direction findPathAStar(MapLocation src, MapLocation dest){
+        
+        return null;
+    }
+    
+}
