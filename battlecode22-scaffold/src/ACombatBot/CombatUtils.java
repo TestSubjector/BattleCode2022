405b00facf6de72c823112111f802b381eed086e
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2022-01-13 12:51:45 -0500

    Reset commit

---
+++ battlecode22-scaffold/src/ACombatBot/CombatUtils.java
@@ -0,0 +1,19 @@
+package ACombatBot;
+
+import battlecode.common.*;
+
+public class CombatUtils extends Util{
+
+    public static boolean isMilitaryUnit(RobotType type){
+        switch (type){
+            case SOLDIER:
+            case WATCHTOWER:
+            case SAGE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    
+}
