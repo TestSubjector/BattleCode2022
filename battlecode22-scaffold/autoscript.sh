36294c87dcef1c5722e7e1f4e42f7d4558de6d3c
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2022-01-14 14:54:58 -0500

    Parallel autoscript

--- battlecode22-scaffold/autoscript.sh
+++ battlecode22-scaffold/autoscript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -o errexit
 
-team1=AByteBot
-team2=AFleeingBot
+team1=AFleeingBot
+team2=OFleeingDummyBot
 
 mkdir -p logs
 
@@ -9,17 +10,32 @@ rm -f logs/results.log
 rm -f logs/warnings.log
 rm -f logs/freezing.log
 rm -f logs/detailed_results.log
+rm -f logs/log1.log
+rm -f logs/log2.log
 rm -f logs/log.log
 
 for i in intersection eckleburg fortress colosseum jellyfish progress rivers \
     valley squer sandwich underground nottestsmall uncomfortable #doubledoors newdoubledoors
+
 do
   echo "Running map $i"
-  ./gradlew -PteamA=$team1 -PteamB=$team2 -Pmaps=$i -PprofilerEnabled=false run >> logs/log.log
-  ./gradlew -PteamA=$team2 -PteamB=$team1 -Pmaps=$i -PprofilerEnabled=false run >> logs/log.log
+  (trap 'kill 0' SIGINT;
+  ./gradlew -PteamA=$team1 -PteamB=$team2 -Pmaps=$i -PprofilerEnabled=false run >> logs/log1.log &
+  ./gradlew -PteamA=$team2 -PteamB=$team1 -Pmaps=$i -PprofilerEnabled=false run >> logs/log2.log
+  )
+  wait
 done
+
 echo "Grepping results"
-grep -F "wins" logs/log.log >> logs/results.log
-grep -E "vs.|wins" logs/log.log >> logs/detailed_results.log
-grep -F "vs.|Warning" logs/log.log >> logs/warnings.log
-grep -F "vs.|Birth round" logs/log.log >> logs/freezing.log
+grep -F "wins" logs/log1.log >> logs/results.log
+echo -e "===========\n" >> logs/results.log
+grep -F "wins" logs/log2.log >> logs/results.log
+grep -E "vs. | wins" logs/log1.log >> logs/detailed_results.log
+echo -e "===========\n" >> logs/results.log
+grep -E "vs. | wins" logs/log2.log >> logs/detailed_results.log
+grep -F "Warning" logs/log1.log >> logs/warnings.log
+echo -e "===========\n" >> logs/results.log
+grep -F "Warning" logs/log2.log >> logs/warnings.log
+grep -F "Birth round" logs/log1.log >> logs/freezing.log
+echo -e "===========\n" >> logs/results.log
+grep -F "Birth round" logs/log2.log >> logs/freezing.log
